dd75e632d8fe8fbae7701beb88d2b10ca9a7b5f8
OlesiaPonomarenko <kukussya2006@gmail.com>
2024-09-02 20:53:27 +0300

    ggg

--- js/task-1.js
+++ js/task-1.js
@@ -1,41 +1,21 @@
-// Перед звільненням розробник зламав
-// вихідний код управління акаунтами користувачів
-// нашого сервісу доставки їжі.Виконай рефакторинг
-// методів об'єкта customer, розставивши відсутні this
-// під час звернення до властивостей об'єкта.
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-// Використай цей стартовий код і виконай рефакторинг.
-// Після оголошення об'єкта ми додали виклики методів.
-//  У консоль будуть виведені результати їх роботи.Будь ласка,
-//   нічого там не змінюй.
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
+// елемента(тегу < h2 >) і кількість елементів у категорії(усіх < li >, вкладених у нього).
 
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+// На що буде звертати увагу ментор при перевірці:
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+// Кількість категорій, їх назва та кількість елементів отримані за допомогою властивостей і методів DOM-елементів
+// Дані за кожною категорією були отримані й виведені в консоль у тілі циклу або методу forEach()
+// У консолі має бути виведено наступне повідомлення:
+
+const categoriesList = document.querySelector("#categories");
+const categoriesItems = categoriesList.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesItems.length}`);
+categoriesItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const elementsCount = item.querySelectorAll("li").length;
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${elementsCount}`);
+});
